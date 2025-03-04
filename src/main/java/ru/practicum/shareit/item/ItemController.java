10e6c5029b19fbca0fc6d6ba134f117bbbed1ee1
Ilya <Em0zzIoN@yandex.ru>
2025-03-04 12:11:39 +0300

    feat: добавить контроллеры и интерфейсы для сервисов

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -1,12 +1,52 @@
 package ru.practicum.shareit.item;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.item.dto.ItemDto;
 
-/**
- * TODO Sprint add-controllers.
- */
+import java.util.List;
+
 @RestController
 @RequestMapping("/items")
 public class ItemController {
+
+    private final ItemService itemService;
+
+    // Конструктор для внедрения сервиса
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    // Добавление новой вещи
+    @PostMapping
+    public ItemDto addItem(@RequestBody ItemDto itemDto,
+                           @RequestHeader("X-Sharer-User-Id") Long ownerId) {
+        return itemService.addItem(itemDto, ownerId);
+    }
+
+    // Редактирование вещи (только владелец)
+    @PatchMapping("/{itemId}")
+    public ItemDto updateItem(@PathVariable Long itemId,
+                              @RequestBody ItemDto itemDto,
+                              @RequestHeader("X-Sharer-User-Id") Long ownerId) {
+        return itemService.updateItem(itemId, itemDto, ownerId);
+    }
+
+    // Получение информации о конкретной вещи
+    @GetMapping("/{itemId}")
+    public ItemDto getItemById(@PathVariable Long itemId,
+                               @RequestHeader("X-Sharer-User-Id") Long requesterId) {
+        return itemService.getItemById(itemId, requesterId);
+    }
+
+    // Получение списка всех вещей владельца
+    @GetMapping
+    public List<ItemDto> getItemsByOwner(@RequestHeader("X-Sharer-User-Id") Long ownerId) {
+        return itemService.getItemsByOwner(ownerId);
+    }
+
+    // Поиск вещей по тексту (возвращаются только доступные для аренды)
+    @GetMapping("/search")
+    public List<ItemDto> searchItems(@RequestParam String text) {
+        return itemService.searchItems(text);
+    }
 }
---
+++ src/main/java/ru/practicum/shareit/user/UserService.java
@@ -0,0 +1,13 @@
+package ru.practicum.shareit.user;
+
+import ru.practicum.shareit.user.dto.UserDto;
+
+import java.util.List;
+
+public interface UserService {
+    UserDto addUser(UserDto userDto);
+    UserDto updateUser(Long userId, UserDto userDto);
+    UserDto getUser(Long userId);
+    List<UserDto> getUsers();
+    void deleteUser(Long userId);
+}
