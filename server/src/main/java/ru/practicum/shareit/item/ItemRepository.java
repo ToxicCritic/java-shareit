28cebff81985488ffde40898151ef9e7c4d2a407
Ilya <Em0zzIoN@yandex.ru>
2025-03-29 17:06:40 +0300

    refactor: checkstyle

--- server/src/main/java/ru/practicum/shareit/item/ItemRepository.java
+++ server/src/main/java/ru/practicum/shareit/item/ItemRepository.java
@@ -5,5 +5,6 @@ import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
     List<Item> findByOwnerId(Long ownerId);
+
     List<Item> findByRequestId(Long requestId);
 }
